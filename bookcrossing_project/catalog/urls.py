154df57f2c8a790fb982bd189513be4ada45431f
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-14 13:52:31 +0300

    feat: Add logic for genres and books detail

--- bookcrossing_project/catalog/urls.py
+++ bookcrossing_project/catalog/urls.py
@@ -7,6 +7,6 @@ app_name = 'catalog'
 urlpatterns = [
     path('', views.index_view, name='index_view'),
     path('categories/', views.categories_view, name='categories_view'),
-    path('categories/<int:id>/', views.category_detail_view, name='category_detail_view'),
+    path('categories/<str:name>/', views.category_detail_view, name='category_detail_view'),
     path('book/<int:id>/', views.book_detail_view, name='book_detail_view'),
 ]
--- bookcrossing_project/catalog/views.py
+++ bookcrossing_project/catalog/views.py
@@ -1,13 +1,26 @@
+from data import BOOKS, CATEGORIES
 from django.shortcuts import render
 
 def index_view(request):
-    return render(request, template_name='catalog/index.html')
+    context = {'books': BOOKS}
+    return render(request, 'catalog/index.html', context)
 
 def categories_view(request):
-    return render(request, template_name='catalog/categories.html')
+    context = {'categories': CATEGORIES}
+    return render(request, template_name='catalog/categories.html', context=context)
 
-def category_detail_view(request, id):
-    return render(request, template_name='catalog/category.html')
+def category_detail_view(request, name):
+    public_books = []
+    for book in BOOKS:
+        if name in book['category']:
+            public_books.append(book)
+    return render(request, template_name='catalog/category.html', context={'books': public_books, 'category': name})
 
 def book_detail_view(request, id):
-    return render(request, template_name='catalog/book.html')
+    context = {}
+    for book in BOOKS:
+        if book['id'] == id:
+            context = {'book': book}
+            return render(request, template_name='catalog/book.html', context={'book':book})
+    # Реализовать проверку на отсутсвие книги по id
+    return render(request, template_name='catalog/index.html', context=context)
