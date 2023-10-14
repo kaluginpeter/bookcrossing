ec66d68b86446f5a6a9afe73665c9c62951dce12
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-14 11:36:30 +0300

    feat: Add name spaces for pathes

--- bookcrossing_project/catalog/urls.py
+++ bookcrossing_project/catalog/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
 from . import views
+
+app_name = 'catalog'
+
 urlpatterns = [
-    path('', views.index_view),
-    path('categories/', views.categories_view),
-    path('categories/<int:id>/', views.category_detail_view),
-    path('book/<int:id>/', views.book_detail_view),
+    path('', views.index_view, name='index_view'),
+    path('categories/', views.categories_view, name='categories_view'),
+    path('categories/<int:id>/', views.category_detail_view, name='category_detail_view'),
+    path('book/<int:id>/', views.book_detail_view, name='book_detail_view'),
 ]
--- bookcrossing_project/pages/urls.py
+++ bookcrossing_project/pages/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'pages'
+
 urlpatterns = [
-    path('', views.about_view),
+    path('', views.about_view, name='about_views'),
 ]
